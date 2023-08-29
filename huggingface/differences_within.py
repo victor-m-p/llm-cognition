0aa94187de41cf3d558d6b04b4c696d326c64452
Victor Poulsen <vmp@pop-os.localdomain>
2023-08-29 01:41:28 -0400

    running some plots on the new data

--- huggingface/differences_within.py
+++ huggingface/differences_within.py
@@ -1,3 +1,11 @@
+'''
+VMP 2023-08-29
+Figure out what happens within generations and across iterations
+(but not across conditions in this script).
+1. similarity against first generation
+3. heatmat of similarity within generations between num
+'''
+
 import pandas as pd 
 import numpy as np 
 import seaborn as sns 
@@ -19,12 +27,12 @@ df = pd.read_csv('../data/data_cleaned/gpt4.csv')
 # sort values
 df = df.sort_values(['condition', 'id', 'iteration', 'num'])
 
-### 1. similarity against first generation ###
-### done for each (condition, id) pair ###
+# all combinations of (id, condition)
 id_list = df['id'].unique()
 condition_list = df['condition'].unique()
 combinations = list(itertools.product(id_list, condition_list))
 
+### 1. similarity against first generation ###
 for id, condition in combinations:
     # get responses
     responses = df[(df['id'] == id) & (df['condition'] == condition)]['response'].tolist()
@@ -63,44 +71,58 @@ for id, condition in combinations:
     plt.title(f'{id} {condition}')
     plt.savefig(f'../fig/gpt4/first_response/{id}_{condition}.png')
 
-# then we take responses out 
-responses = df['response'].tolist()
+### 3. heatmat of similarity within generations between num ###
+for id, condition in combinations:
+    # get responses
+    responses = df[(df['id'] == id) & (df['condition'] == condition)]['response'].tolist()
+    # encode sentences (see helper_functions.py)
+    encodings = encode_responses(tokenizer, responses)
+    # embed responses (see helper_functions.py)
+    embeddings = embed_responses(model, encodings)
+    # reshape embeddings
+    num_rows = embeddings.shape[0]
+    num_iterations = int(num_rows / num_per_iteration)
+    reshaped_embeddings = embeddings.reshape(num_iterations, num_per_iteration, 384)
+    # easier to divide with sum than to take mean
+    sum_distances = np.zeros((num_per_iteration, num_per_iteration))
 
-# encode sentences (see helper_functions.py)
-encodings = encode_responses(tokenizer, responses)
+    # Loop through the 41 iterations to accumulate the sum of cosine distances for each pair
+    for iter_idx in range(num_iterations):
+        distances = cosine_distances(reshaped_embeddings[iter_idx, :, :])
+        sum_distances += distances
 
-# embed responses (see helper_functions.py)
-embeddings = embed_responses(model, encodings)
+    # Calculate the mean distances
+    mean_distances = sum_distances / num_iterations
 
-### 1. how similar is each first, second, n element to each other ###
+    # Create the DataFrame to store mean distances
+    froms = []
+    tos = []
+    mean_distances_list = []
 
-num_per_iteration = 20
-num_rows = embeddings.shape[0]
-num_iterations = int(num_rows / num_per_iteration)
-reshaped_embeddings = embeddings.reshape(num_iterations, num_per_iteration, 384)
-mean_cosine_distances = []
-nums = []
-froms = []
-tos = []
-cosine_distances_list = []
-for i in range(num_per_iteration):
-    col_elements = reshaped_embeddings[:, i, :]
-    distances = cosine_distances(col_elements)
-    
-    for from_idx in range(len(distances)):
-        for to_idx in range(len(distances)):
-            nums.append(i+1) # +1 just for prettier plot
+    for from_idx in range(num_per_iteration):
+        for to_idx in range(num_per_iteration):
             froms.append(from_idx)
             tos.append(to_idx)
-            cosine_distances_list.append(distances[from_idx, to_idx])
+            mean_distances_list.append(mean_distances[from_idx, to_idx])
 
-# Create the DataFrame
-df = pd.DataFrame({
-    'num': nums,
-    'from': froms,
-    'to': tos,
-    'cosine_distance': cosine_distances_list
-})
+    df_ = pd.DataFrame({
+        'from': froms,
+        'to': tos,
+        'mean_distance': mean_distances_list
+    })
 
-import seaborn as sns 
-sns.boxplot(x='num', y='cosine_distance', data=df)
+    import matplotlib.pyplot as plt 
+
+    heatmap_data = df_.pivot(index="from", 
+                            columns="to", 
+                            values="mean_distance")
+
+    mask = np.tril(np.ones_like(heatmap_data, dtype=bool), k=-1)
+
+    # Generate the heatmap
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', cbar=True, mask=mask)
+    plt.title(f"{id} {condition}")
+    plt.xlabel("")
+    plt.ylabel("")
+    plt.savefig(f'../fig/gpt4/heatmap/{id}_{condition}.png')
